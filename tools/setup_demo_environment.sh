1355d17182ec0fe25f68f82679933feff4a7a5b9
Juergen Buddy Hoffmann <jhoffmann@redhat.com>
2013-02-12 16:05:20 +0100

    more info after the installation updated the template file

--- tools/setup_demo_environment.sh
+++ tools/setup_demo_environment.sh
@@ -78,6 +78,8 @@ mkdir /tmp/svn-work
 cd /tmp/svn-work
 svn --no-auth-cache --username test --password redhat checkout http://localhost/svn/test/trunk/jboss-soe/
 
+clear
+
 cat <<EOF
 Congratulations, Your working copy has been created in /tmp/svn-work
 You should start by editing your build.properties in /tmp/svn-work/jboss-soe/build
@@ -93,4 +95,35 @@ target.installation.directory=/opt
 NOTE!!!
 If you do not want to use the RHN Channels for the JBoss EAP6 Version you should download
 https://access.redhat.com/jbossnetwork/restricted/softwareDownload.html?softwareId=17633&product=appplatform
+unzip it and put it into
+
+/tmp/svn-work/jboss-soe/jboss-eap6-master
+
+so that ls -la /tmp/svn-work/jboss-soe/jboss-eap6-master prints
+
+-rw-rw-r--@  1 buddy  staff     425 20 Nov 15:54 JBossEULA.txt
+-rw-rw-r--@  1 buddy  staff   26530 20 Nov 15:54 LICENSE.txt
+drwxrwxr-x@  3 buddy  staff     102 20 Nov 15:54 appclient
+drwxrwxr-x@ 35 buddy  staff    1190 20 Dez 13:16 bin
+drwxrwxr-x@  4 buddy  staff     136 20 Nov 15:54 bundles
+drwxrwxr-x@  5 buddy  staff     170 20 Nov 15:54 docs
+drwxrwxr-x@  6 buddy  staff     204 18 Jan 12:13 domain
+-rw-rw-r--@  1 buddy  staff  267443 20 Nov 15:54 jboss-modules.jar
+drwxrwxr-x@ 14 buddy  staff     476 20 Nov 15:54 modules
+drwxrwxr-x@  8 buddy  staff     272 17 Jan 13:41 standalone
+-rw-rw-r--@  1 buddy  staff      58 20 Nov 15:54 version.txt
+drwxrwxr-x@ 10 buddy  staff     340 20 Dez 13:16 welcome-content
+
+and uncomment the option
+
+#master.revision.specfile.name=base-jboss-eap6.spec
+#profile.requires.upstream=jboss-eap-base
+#profile.requires.upstream.version=${eap.version}
+
+and comment
+
+profile.requires.upstream=jbossas-product-eap
+profile.requires.upstream.version= >= 7.1.2
+
+
 EOF
